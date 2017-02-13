ee17464ccd114f10272b7e22e5ae3e4ef5ffc861
Nico Malig <nicomalig@gmail.com>
2017-02-13 11:08:37 -0800

    partial complete inclass

--- nhanes-analysis/activity.R
+++ nhanes-analysis/activity.R
@@ -1,7 +1,7 @@
 # Calculate survey weighted estimates
 
 # Set up
-setwd('~/Documents/info-498c/demos/nhanes-analysis/')
+
 # library(foreign)
 library(survey)
 library(dplyr)
@@ -17,13 +17,14 @@ alcohol <- sasxport.get('./data/ALQ_H.XPT')
 nhanes <- left_join(alcohol, demographics, by='seqn')
 
 # Take the sum of the weighting column `wtint2yr` - what is this number?
-
+pop <- sum(nhanes$wtint2yr)
+# # This number is population of the survey
 
 # Create a survey design that indicates the id, strata, and weights
-
+design <- svydesign(id = ~nhanes$seqn, strata = ~nhanes$sdmvstra, weight = ~nhanes$wtint2yr)
 
 # Using the codebook, find the question that asks about 12+ drinks in the last year
-
+# # Looks like this is question ALQ101
 
 # Using the `table` function get a table of 12+ drinks in the last year responses
 
